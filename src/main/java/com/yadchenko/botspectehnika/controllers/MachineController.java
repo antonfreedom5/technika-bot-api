2dddb45173c37988c5846fc83bdb1a39ded5e8b3
Anton Yadchenko <antonfreedom5@gmail.com>
2024-03-05 08:47:30 +0300

    added logs for open bot

--- src/main/java/com/yadchenko/botspectehnika/controllers/MachineController.java
+++ src/main/java/com/yadchenko/botspectehnika/controllers/MachineController.java
@@ -3,13 +3,12 @@ package com.yadchenko.botspectehnika.controllers;
 import com.yadchenko.botspectehnika.entities.Machine;
 import com.yadchenko.botspectehnika.services.MachineService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/machines")
@@ -18,7 +17,8 @@ public class MachineController {
     private final MachineService machineService;
 
     @GetMapping("/all")
-    List<Machine> getAll() {
+    List<Machine> getAll(@RequestHeader(value="User-Id") String userId) {
+        log.info("User with id {} opened the bot", userId);
         return machineService.getAll();
     }
 }
